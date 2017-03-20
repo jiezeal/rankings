5c2c2abb8332089928834535a424b0a0d0a1b8ff
zhulinjie <zhulinjie_cool@126.com>
2017-03-20 17:04:45 +0800

    修复点赞详情报错的BUG

--- app/Presenters/RankingPresenter.php
+++ app/Presenters/RankingPresenter.php
@@ -21,14 +21,17 @@ class RankingPresenter
     }
 
     /**
+     * 点赞排行
      * @param $ranking
      * @return mixed
      */
     public function discussion($ranking){
         // 判断是否是从缓存中获取的数据
         if(isset($ranking->id)){
-            // 从缓存中取
-            $user = unserialize($this->rankingCache->get(STRING_USER_ . $ranking->user_id));
+            // 从string缓存中取
+//            $user = unserialize($this->rankingCache->get(STRING_USER_ . $ranking->user_id));
+            // 从hash缓存中获取
+            $user = (object)$this->rankingCache->hgetall(HASH_USER_ . $ranking->user_id);
             $ranking->avatar = $user->avatar;
             $ranking->name = $user->name;
             return $ranking;
@@ -39,6 +42,27 @@ class RankingPresenter
             $discussion->avatar = $user->avatar;
             $discussion->name = $user->name;
             return $discussion;
+        }
+    }
+
+    /**
+     * 获取赞过这篇帖子的用户
+     * @param $discussion
+     * @return array
+     */
+    public function rankings($discussion){
+        // 从缓存中获取赞过这篇帖子的用户
+        if($this->rankingCache->exists(SADD_DISCUSSION_ . $discussion->id)){
+            $ids = $this->rankingCache->smembers(SADD_DISCUSSION_ . $discussion->id);
+            $rankings = [];
+            foreach($ids as $id){
+                // 从hash缓存中获取
+                $rankings[] = (object)$this->rankingCache->hgetall(HASH_USER_ . $id);
+            }
+            return $rankings;
+        // 从数据库中获取赞过这篇帖子的用户
+        }else{
+            return $discussion->rankings;
         }
     }
 }
--- resources/views/web/ranking/show.blade.php
+++ resources/views/web/ranking/show.blade.php
@@ -1,27 +1,29 @@
 @extends('web.app')
 @section('content')
     <div class="jumbotron">
+        @inject('discussionPresenter', 'App\Presenters\DiscussionPresenter')
         <div class="container forum-show">
             <div class="media">
                 <div class="media-left">
                     <a href="#">
-                        <img class="media-object img-circle avatar" src="{{ $discussion->user->avatar }}" alt="">
+                        <img class="media-object img-circle avatar" src="{{ $discussionPresenter->user($discussion)->avatar }}" alt="">
                     </a>
                 </div>
                 <div class="media-body">
                     <h4 class="media-heading">{{ $discussion->title }}</h4>
-                    {{ $discussion->user->name }}
+                    {{ $discussionPresenter->user($discussion)->name }}
                 </div>
             </div>
         </div>
     </div>
     <div class="container ranking-show">
+        @inject('rankingPresenter', 'App\Presenters\RankingPresenter')
         <div class="row">
             <div class="col-md-12" role="main">
-                @forelse($discussion->rankings as $ranking)
+                @forelse($rankingPresenter->rankings($discussion) as $ranking)
                     <dl>
-                        <dt><img class="avatar img-circle" src="{{ $ranking->user->avatar }}" alt=""></dt>
-                        <dd>{{ $ranking->user->name }}</dd>
+                        <dt><img class="avatar img-circle" src="{{ $ranking->avatar }}" alt=""></dt>
+                        <dd>{{ $ranking->name }}</dd>
                     </dl>
                 @empty
                     暂无数据
